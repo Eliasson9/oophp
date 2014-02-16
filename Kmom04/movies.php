6e0a7e953525c02ad33a97c5a193d0970ef47fcd
patrik <patrik@patrik-Lenovo-IdeaPad-Y510P.(none)>
2014-02-16 17:13:04 +0100

    small change

--- Kmom04/movies.php
+++ Kmom04/movies.php
@@ -2,6 +2,20 @@
 
 include(__DIR__.'/config.php');
 
+/**
+* Function to create links for sorting
+*
+* @param string $column the name of the database column to sort by
+* @return string with links to order by column.
+*/
+function orderBy($column) {
+	return "<span class='orderBy'><a href='?orderBy={$column}&order=asc'>&darr;</i></a><a href='?orderby={$column}&order=desc'>&uarr;</a></span>";
+}
+
+$dbConn = new CDatabase();
+dump($dbConn->getRes());
+$shire['stylesheets'][] = 'css/form.css';
+
 $shire['title'] = "Me-Sida";
 
 $shire['main'] = <<<EOD
--- Shire/src/CDatabase/CDatabase.php
+++ Shire/src/CDatabase/CDatabase.php
@@ -1,18 +1,40 @@
 <?php
 
 /**
-*	This calss handle the database connection
-**/
+*	This calss handle the database connection and functions related to the database
+*/
 
 class CDatabase {
 	/**
 	*	parameters
-	**/	
+	*/	
+	private $pdo;
 	
 	/**
 	*	constructor
-	**/	
+	*/	
 	public function __construct() {
-		
+		$dsn      = 'mysql:host=localhost;dbname=Movies;';
+		$login    = 'root';
+		$password = '';
+		$options  = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'");
+		try {
+  			$this->pdo = new PDO($dsn, $login, $password, $options);
+			$this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+		}
+		catch(Exception $e) {
+		  //throw $e; // For debug purpose, shows all connection details
+		  throw new PDOException('Could not connect to database, hiding connection details.'); // Hide connection details.
+		}
 	}
+	
+	public function getRes() {
+		$sql = "SELECT * FROM Movie;";
+		$sth = $this->pdo->prepare($sql);
+		$sth->execute();
+		$res = $sth->fetchAll();
+		return $res;
+	}
+	
+	
 }
