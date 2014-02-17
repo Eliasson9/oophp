c5160b042a7037d6bea595fb7187bcad1c575921
patrik <patrik@patrik-Lenovo-IdeaPad-Y510P.(none)>
2014-02-17 13:42:35 +0100

    gay

--- Kmom04/movies.php
+++ Kmom04/movies.php
@@ -2,27 +2,61 @@
 
 include(__DIR__.'/config.php');
 
-/**
-* Function to create links for sorting
-*
-* @param string $column the name of the database column to sort by
-* @return string with links to order by column.
-*/
-function orderBy($column) {
-	return "<span class='orderBy'><a href='?orderBy={$column}&order=asc'>&darr;</i></a><a href='?orderby={$column}&order=desc'>&uarr;</a></span>";
-}
-
 $db = new CDatabase($shire['database']);
 $sql = "SELECT COUNT(id) FROM VMovie";
 $res = $db->ExecuteSelectQueryAndFetchAll($sql);
-echo $db->Dump();
+$db->Dump();
+dump($res);
+$hits = isset($_GET['hits']);
+$page = isset($_GET['page']);
+$max = 5;
+$orderby = isset($_GET['orderby']) ? strtolower($_GET['orderby']) : 'id';
+$order = isset($_GET['order']) ? strtolower($_GET['order']) : 'asc';
+$year1 = isset($_GET['year1']) && !empty($_GET['year1']) ? $_GET['year1'] : null;
+$year2 = isset($_GET['year2']) && !empty($_GET['year2']) ? $_GET['year2'] : null;
+dump($max[0]);
+in_array($orderby, array('id', 'title', 'year')) or die('Check: Not valid column.');
+in_array($order, array('asc', 'desc')) or die('Check: Not valid sort order.');
+
+$sql = "SELECT * FROM VMovie ORDER BY $orderby $order";
+$res = $db->ExecuteSelectQueryAndFetchAll($sql);
+
+$sql = "SELECT * FROM VMovie LIMIT $hits OFFSET " . (($page - 1) * $hits);
+$res = $db->ExecuteSelectQueryAndFetchAll($sql);
+
+$tr = "<table>\n<tr><th>Rad</th><th>Id " . $db->orderBy('id') . "</th><th>Bild</th><th>Titel " . $db->orderBy('title') . "</th><th>År " . $db->orderBy('year') . "</th><th>Genre</th></tr>\n";
+foreach ($res as $key => $movie) {
+	$tr .= "<tr>\n";
+	$tr .= "<td>{$key}</td><td>{$movie->id}</td><td><img class='movieImg' src='{$movie->image}' alt='movieImage'></td><td>{$movie->title}</td><td>{$movie->year}</td><td>$movie->genre</td>\n";
+	$tr .= "</tr>\n";
+}
+$tr .= "</table>\n";
+
 
 $shire['stylesheets'][] = 'css/form.css';
+$shire['stylesheets'][] = 'css/table.css';
 
 $shire['title'] = "Me-Sida";
 
 $shire['main'] = <<<EOD
-
+<form>
+	<fieldset>
+		<legend>Sök</legend>
+		<p><label>Skapad mellan åren: 
+    	<input type='text' name='year1' value='{$year1}'/> - <input type='text' name='year2' value='{$year2}'/>
+  		</label>
+		</p>
+		<p><input type='submit' name='submit' value='Sök'/></p>
+		<p><a href='?'>Visa alla</a></p>
+	</fieldset>
+</form>
+<div id='above'>
+	{$db->getHitsPerPage(array(2, 4, 6))}
+</div>
+{$tr}
+<div id='below'>
+	{$db->getPageNav($hits, $page, $max)}
+</div>
 EOD;
 
 include(SHIRE_THEME_PATH);
--- Shire/src/CDatabase/CDatabase.php
+++ Shire/src/CDatabase/CDatabase.php
@@ -26,20 +26,28 @@ class CDatabase {
       	'password' => null,
       	'driver_options' => null,
       	'fetch_style' => PDO::FETCH_OBJ,
- 	);
-    $this->options = array_merge($default, $options);
+ 		);
+    	$this->options = array_merge($default, $options);
  
-    try {
-    	$this->db = new PDO($this->options['dsn'], $this->options['username'], $this->options['password'], $this->options['driver_options']);
-    }
-    catch(Exception $e) {
-    	//throw $e; // For debug purpose, shows all connection details
-      	throw new PDOException('Could not connect to database, hiding connection details.'	); // Hide connection details.
-    }
+    	try {
+    		$this->db = new PDO($this->options['dsn'], $this->options['username'], $this->options['password'], $this->options['driver_options']);
+    	}
+    	catch(Exception $e) {
+    		//throw $e; // For debug purpose, shows all connection details
+      		throw new PDOException('Could not connect to database, hiding connection details.'	); // Hide connection details.
+    	}
  
     	$this->db->SetAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, $this->options['fetch_style']); 
   	}
 	
+	/**
+	 * Function that execute an string and fetch it.
+	 * 
+	 * @param string $query the query sql
+	 * @param array $params contains argument that replace ?
+	 * @param boolean $debug check if debug is needed
+	 * @return array with the resultset
+	 */
 	public function ExecuteSelectQueryAndFetchAll($query, $params=array(), $debug=false) {
  
     	self::$queries[] = $query; 
@@ -69,5 +77,69 @@ class CDatabase {
     	return $html . '</pre>';
   	}
 	
+	/**
+	* Function to create links for sorting
+	*
+	* @param string $column the name of the database column to sort by
+	* @return string with links to order by column.
+	*/
+	function orderBy($column) {
+		return "<span class='orderBy'><a href='?orderBy={$column}&order=asc'>&darr;</i></a><a href='?orderby={$column}&order=desc'>&uarr;</a></span>";
+	}
+	
+	/**
+ 	* Use the current querystring as base, modify it according to $options and return the modified query string.
+ 	*
+ 	* @param array $options to set/change.
+ 	* @param string $prepend this to the resulting query string
+ 	* @return string with an updated query string.
+ 	*/
+	function getQueryString($options, $prepend='?') {
+  		// parse query string into array
+  		$query = array();
+  		parse_str($_SERVER['QUERY_STRING'], $query);
+ 
+  		// Modify the existing query string with new options
+  		$query = array_merge($query, $options);
+ 
+  		// Return the modified querystring
+  		return $prepend . http_build_query($query);
+	}
+	
+	/**
+ 	* Create links for hits per page.
+ 	*
+ 	* @param array $hits a list of hits-options to display.
+ 	* @return string as a link to this page.
+ 	*/
+	function getHitsPerPage($hits) {
+  		$nav = "Träffar per sida: ";
+  		foreach($hits AS $val) {
+    		$nav .= "<a href='" . $this->getQueryString(array('hits' => $val)) . "'>$val</a> ";
+  		}  
+  		return $nav;
+	}
+	
+	/**
+ 	* Create navigation among pages.
+ 	*
+ 	* @param integer $hits per page.
+ 	* @param integer $page current page.
+ 	* @param integer $max number of pages. 
+ 	* @param integer $min is the first page number, usually 0 or 1. 
+ 	* @return string as a link to this page.
+ 	*/
+	function getPageNav($hits, $page, $max, $min=1) {
+  		$nav  = "<a href='" . $this->getQueryString(array('page' => $min)) . "'>&lt;&lt;</a> ";
+  		$nav .= "<a href='" . $this->getQueryString(array('page' => ($page > $min ? $page - 1 : $min) )) . "'>&lt;</a> ";
+ 
+  		for($i=$min; $i<=$max; $i++) {
+    		$nav .= "<a href='" . $this->getQueryString(array('page' => $i)) . "'>$i</a> ";
+  		}
+ 	
+  		$nav .= "<a href='" . $this->getQueryString(array('page' => ($page < $max ? $page + 1 : $max) )) . "'>&gt;</a> ";
+  		$nav .= "<a href='" . $this->getQueryString(array('page' => $max)) . "'>&gt;&gt;</a> ";
+  		return $nav;
+	}
 	
 }
