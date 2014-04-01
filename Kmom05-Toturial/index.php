a7c674c68d20796a76923f86a172764cb393f606
patrik <patrik@patrik-Lenovo-IdeaPad-Y510P.(none)>
2014-04-01 23:56:50 +0200

    fixed a part of Form Update

--- Kmom05-Toturial/index.php
+++ Kmom05-Toturial/index.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 include __DIR__.'/config.php';
 
@@ -8,16 +8,45 @@ $shire['stylesheets'][] = 'css/source.css';
 //Connect to database
 $db = new CDatabase($shire['database']);
 
+/**
+ * Create a link to the content, based on its type.
+ *
+ * @param object $content to link to.
+ * @return string with url to display content.
+ */
+function getUrlToContent($site) {
+  	switch($site->TYPE) {
+    	case 'page': 
+    		return "page.php?url={$site->url}"; 
+    	break;
+	    case 'post': 
+	    	return "blog.php?slug={$site->slug}"; 
+	    break;
+	    default: 
+	    	return null; 
+	    break;
+  	}
+}
 
 $sql = "SELECT *, (published <= NOW()) AS available FROM Content;";
 $res = $db->ExecuteSelectQueryAndFetchAll($sql);
-var_dump($res);
 
+$html = "";
+$html .= "<ul>";
+foreach ($res as $key => $site) {
+	$html .= "<li>";	
+		$html .= "Type: ".$site->TYPE."<br> Title: ".$site->title." (<a href='update.php?id={$site->id}'>Editera</a> <a href='".getUrlToContent($site)."'>Visa</a>)";
+	$html .= "</li><br>";
+}
+$html .= "</ul>";
 
 
 // Do it and store it all in variables in the Shire container.
 $shire['title'] = "Visa Innehåll";
 
-$shire['main'] = "<h1>Visa Innehåll</h1>\n";
+$shire['main'] = <<<EOD
+<h1>Visa Innehåll</h1> 
+{$html}
+EOD;
 
 include SHIRE_THEME_PATH;
---
+++ Kmom05-Toturial/update.php
@@ -0,0 +1,84 @@
+<?php
+
+include __DIR__.'/config.php';
+
+// Add style for csource
+$shire['stylesheets'][] = 'css/source.css';
+$shire['stylesheets'][] = 'css/form.css';
+
+//Connect to database
+$db = new CDatabase($shire['database']);
+$params = array();
+$sql = "";
+$html = "";
+$id = isset($_POST['id']) ? strip_tags($_POST['id']) : isset($_GET['id']) ? strip_tags($_GET['id']) : null; 
+is_numeric($id) or die('Check: Id must be numeric.');
+
+if(isset($_POST['edit'])) {
+	$title = isset($_POST['title']) ? $_POST['title'] : null;
+	$slug = isset($_POST['slug']) ? $_POST['slug'] : null;
+	$url = isset($_POST['url']) ? $_POST['url'] : null;
+	$type = isset($_POST['TYPE']) ? $_POST['TYPE'] : null;
+	$filter = isset($_POST['filter']) ? $_POST['filter'] : null;
+	$published = isset($_POST['published']) ? $_POST['published'] : null;
+	$data = isset($_POST['DATA']) ? $_POST['DATA'] : null;
+	
+	$sql = "UPDATE Content SET slug = ?, url = ?, TYPE = ?, title = ? DATA = ?, FILTER = ?, published = ?, updated = NOW() WHERE id = ?;";
+	$params[] = array($slug, $url, $type, $title,  $data, $filter, published, $id);
+	$db->ExecuteQuery($sql, $params);	
+}
+
+if(isset($_GET['id'])) {
+	$params[] = $id;
+	$sql = "SELECT *, (published <= NOW()) AS available FROM Content WHERE id = ?;";
+	$res = $db->ExecuteSelectQueryAndFetchAll($sql, $params);
+} 
+else {
+	header('Location: index.php');
+	exit;	
+}
+
+// Do it and store it all in variables in the Shire container.
+$shire['title'] = "Updatera Innehåll";
+
+$shire['main'] = <<<EOD
+<form method='POST'>
+	<fieldset>
+		<legend>Uppdatera Innehåll</legend>
+		<input type='hidden' name='id' value='{$res[0]->id}'/>
+		<p>
+			<label>Titel: </label>
+			<input type='text' name='title' value='{$res[0]->title}'/>
+		</p>
+		<p>
+			<label>Slug: </label>
+			<input type='text' name='slug' value='{$res[0]->slug}'/>
+		</p>
+		<p>
+			<label>URL: </label>
+			<input type='text' name='url' value='{$res[0]->url}'/>
+		</p>
+		<p>
+			<label>Type: </label>
+			<input type='text' name='TYPE' value='{$res[0]->TYPE}'/>
+		</p>
+		<p>
+			<label>Filter: </label>
+			<input type='text' name='filter' value='{$res[0]->FILTER}'/>
+		</p>
+		<p>
+			<label>Publisering: </label>
+			<input type='text' name='published' value='{$res[0]->available}'/>
+		</p>
+		<p>
+			<label>Data: </label>
+			<textarea name='data'>{$res[0]->DATA}</textarea>
+		</p>
+		<p>
+			<input type='submit' name='edit' value='Editera'/>
+		</p>
+	</fieldset>
+</form>
+EOD;
+
+include SHIRE_THEME_PATH;
